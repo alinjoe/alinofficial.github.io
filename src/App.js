b71d0c7d4d2e32aa9ae5e521f97e89a95d4accd5
Alin Joe JA <alinjoe98@gmail.com>
2023-07-17 15:28:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,HashRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './components/Layout';
 import About from './components/About'
@@ -8,15 +8,15 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
            <Route index element={<Home />} />
            <Route path="about" element={<About />} />
-           
+           <Route path="/contact" element={<Contact />} />
            </Route>
       </Routes>
-   
+   </HashRouter>
   );
 }
 
