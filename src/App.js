953ecaeaff0ec41cbb11f211c2f604394d895188
Alin Joe JA <alinjoe98@gmail.com>
2023-07-17 12:17:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="alinofficial.github.io" element={<Layout />}>
            <Route index element={<Home />} />
            <Route path="about" element={<About />} />
-           <Route path="/contact" element={<Contact />} />
+           
            </Route>
       </Routes>
    
